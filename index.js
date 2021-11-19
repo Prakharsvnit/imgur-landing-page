a7a159d062e89071dcea75a3326f0b057811bdaa
Prakharsvnit <tprakhar468@gmail.com>
2021-11-19 13:13:34 +0530

    second commit

--- index.js
+++ index.js
@@ -1,29 +1,40 @@
-// const fetchPhotos = () => {
-//     axios.get('https://api.unsplash.com/photos/', {
-//       params: {
-//         client_id: "kAIDL8jzLGf34v9CZoWZXiSdMzplmQilway18VyJhYQ",
-//         page:1,
-//         per_page:30,
-//         order_by:"latest"
-//       }
-//   }).then(response => {
-//       const photos = response.data
-//     ;
-//   })
-//   .catch(function (error) {
-//     console.log(error);
-//   })
-//   .then(function () {
-//     console.log("done");
-//   });
+const fetchPhotos = () => {
+    axios.get('https://api.unsplash.com/photos/', {
+      params: {
+        client_id: "kAIDL8jzLGf34v9CZoWZXiSdMzplmQilway18VyJhYQ",
+        page:1,
+        per_page:50,
+        order_by:"latest"
+      }
+  }).then(response => {
+       const photos = response.data
+       photos.forEach(function(el){
+        appendPhotos(el)
+    });
+  })
+  .catch(function (error) {
+    console.log(error);
+  })
+  .then(function () {
+    console.log("done");
+  });
 
-// }
+}
+
+fetchPhotos();
+
+var data_div = document.getElementById('gallery-item');
+
+function appendPhotos(el){
+
+    var image = document.createElement('img');
+
+    image.src = el.urls.thumb;
+
+    image.classList.add('img-thumb');
+
+    data_div.append(image)
 
 
 
-
-
-
-
-
-
+}
