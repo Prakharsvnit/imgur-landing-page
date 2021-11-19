f3eed9c3f09868f8f3a7e58b0524bf726fb5bd9e
Prakharsvnit <tprakhar468@gmail.com>
2021-11-19 09:53:48 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+// const fetchPhotos = () => {
+//     axios.get('https://api.unsplash.com/photos/', {
+//       params: {
+//         client_id: "kAIDL8jzLGf34v9CZoWZXiSdMzplmQilway18VyJhYQ",
+//         page:1,
+//         per_page:30,
+//         order_by:"latest"
+//       }
+//   }).then(response => {
+//       const photos = response.data
+//     ;
+//   })
+//   .catch(function (error) {
+//     console.log(error);
+//   })
+//   .then(function () {
+//     console.log("done");
+//   });
+
+// }
+
+
+
+
+
+
+
+
+
